eea8d375ad67203b19b783eca7b2adb2b424dae1
eaharonoff <emanuel.aharonoff@gmail.com>
2016-09-08 13:34:27 -0400

    rails completed

--- rails-sinatra/users_controller.rb
+++ rails-sinatra/users_controller.rb
@@ -2,21 +2,49 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    erb :new 
   end
   # /users/new
 
-  # create
+  def create
+    @user = User.create(:name => params["name"], :hometown => params["hometown"])
+    redirect to 'users/#{@user.id}'
+  end
+  #POST /users
+  
+  def index
+    @users = User.all
+    erb :index
+  end
+  #GET /users
 
-  # index
+  def show
+    @user = User.find_by(:id => params[:id])
+    erb :show 
+  end
+  #GET /users/:id
 
-  # show
+  def edit
+    @user = User.find_by(:id => params[:id])
+    erb :edit 
+  end
+  #GET /users/:id/edit
 
-  # edit
+  def update
+    @user = User.find_by(:id => params[:id])
+    @user.name = params["name"]
+    @user.hometown = params["hometown"]
+    @user.save
+    redirect to '/users/#{@user.id}'
+  end
+  #PATCH /users/:id
 
-  # update
-
-  # destroy
-
+  def destroy
+    @user = User.find_by(:id => params[:id])
+    @user.delete
+    redirect to '/users'
+  end
+  #'/users/:id/delete'
 end
 
 
